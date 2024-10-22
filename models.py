eaba788ed67655f8bdd07caa30713470063d5e4c
tenessy0570 <tenessy0570@gmail.com>
2024-10-22 14:35:25 +0700

    updated pydantic models

---
+++ models.py
@@ -0,0 +1,15 @@
+import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel
+
+
+class BtagStatisticsResponseModel(BaseModel):
+    btag: str
+    from_: datetime.datetime
+    to: datetime.datetime
+    registrations_count: int
+    ftds_count: int
+    ftds_summary: Decimal
+    deposits_count: int
+    deposits_summary: Decimal
